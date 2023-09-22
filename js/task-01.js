6e4058eb3cd6fab9b32ea183e618a07e21162938
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-09-22 23:32:24 +0300

    redo task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,8 @@
-const numberOfCategories = document.querySelectorAll(".item");
-const number = [...numberOfCategories];
-console.log("Number of Categories: ", number.length);
-const animalsTitle = number[0].children[0].childNodes[0].textContent;
-console.log("Category: ", animalsTitle);
-const animalsList = [number[0].children[1]];
-const animalsElements = [];
-animalsList.forEach((animal) => animalsElements.push(...animal.children));
-console.log("Elements: ", animalsElements.length);
-const productsTitle = number[1].children[0].childNodes[0].textContent;
-console.log("Category: ", productsTitle);
-const productList = [number[1].children[1]];
-const productElements = [];
-productList.forEach((product) => productElements.push(...product.children));
-console.log("Elements: ", productElements.length);
-const technologiesTitle = number[2].children[0].childNodes[0].textContent;
-console.log("Category: ", technologiesTitle);
-const technologiesList = [number[2].children[1]];
-const technologiesElements = [];
-technologiesList.forEach((techology) =>
-  technologiesElements.push(...techology.children)
-);
-console.log("Elements: ", technologiesElements.length);
+const numberOfCategories = document.querySelector("#categories");
+const categories = [...numberOfCategories.children];
+console.log("Number of categories: ", categories.length);
+categories.forEach((category) => {
+  const categoriesArray = category.children;
+  console.log("Category: ", categoriesArray[0].textContent);
+  console.log("Elements: ", categoriesArray[1].children.length);
+});
