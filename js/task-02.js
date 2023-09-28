d301fd4f7fc9fe5cf8b9a48bd33a511ea4252e6a
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-09-28 23:38:20 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const container = document.querySelector("#ingredients");
+const arrLi = [];
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.className = "item";
+  arrLi.push(li);
+});
+container.append(...arrLi);
