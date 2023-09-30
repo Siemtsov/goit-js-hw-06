01c935892035f62a9dd9a5c80cc2d7e3c2914cef
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-09-30 22:45:43 +0300

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#validation-input");
+input.addEventListener("blur", () => {
+  const dataLength = Number(input.dataset.length);
+  const inputLength = input.value.length;
+  if (inputLength === dataLength) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+});
