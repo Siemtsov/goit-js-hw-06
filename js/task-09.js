cc073b5e849fcd34abfbe5d5836c61b3fe1ce3a6
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-01 11:55:01 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+button.addEventListener("click", changeColor);
+function changeColor(evt) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+}
